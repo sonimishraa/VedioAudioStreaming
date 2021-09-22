5e1971a39727c58b5e73dd87b918613a554685e2
soni mishra <soni.mishra@tamasha.live>
2021-09-22 13:30:13 +0530

    last 22/09/201

--- app/src/main/java/com/tamasha/vedioaudiostreamingapp/uis/HomeActivity.kt
+++ app/src/main/java/com/tamasha/vedioaudiostreamingapp/uis/HomeActivity.kt
@@ -2,11 +2,15 @@ package com.tamasha.vedioaudiostreamingapp.uis
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationView
 import com.tamasha.vedioaudiostreamingapp.R
 import com.tamasha.vedioaudiostreamingapp.databinding.ActivityHomeBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -14,22 +18,45 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class HomeActivity : AppCompatActivity() {
     private lateinit var binding: ActivityHomeBinding
+    lateinit var appBarConfig:AppBarConfiguration
+    lateinit var drawerLayout:DrawerLayout
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val navView: BottomNavigationView = binding.navView
+         drawerLayout = binding.drawerLayout
+        val navView: NavigationView = binding.navView
+        val bottomNavView: BottomNavigationView = binding.bottomNavView
 
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(
+       /* val appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.navigation_home, R.id.navigation_discover, R.id.navigation_play,R.id.navigation_club,R.id.navigation_refresh
-            )
+                R.id.navigation_home,
+                R.id.navigation_discover,
+                R.id.navigation_play,
+                R.id.navigation_club,
+                R.id.navigation_refresh
+            ), drawerLayout
         )
-        setupActionBarWithNavController(navController, appBarConfiguration)
+        setupActionBarWithNavController(navController, appBarConfiguration)*/
+        bottomNavView.setupWithNavController(navController)
         navView.setupWithNavController(navController)
+
     }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment_activity_main)
+        return  navController.navigateUp(appBarConfig)
+    }
+
+   /* override fun onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
+    }*/
 }
